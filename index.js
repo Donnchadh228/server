962b16ffe9bc928bbd8a98acc3e53fd64e2f6ddc
Donnchadh228 <117577537+Donnchadh228@users.noreply.github.com>
2022-11-06 23:15:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 require('dotenv').config()
-console.log("asdasdasdsad")
+
 const express = require('express')
-const sequelize=require('./db.js')
-const models = require('./models/models.js')
-const cors = require('cors')
-const fileUpload = require('express-fileupload')
-const router = require('./router/index.js')
-const ErrorHandler = require('./middleware/ErrorHandllingMiddleware.js')
-const path = require('path')
+// const sequelize=require('./db.js')
+// const models = require('./models/models.js')
+// const cors = require('cors')
+// const fileUpload = require('express-fileupload')
+// const router = require('./router/index.js')
+// const ErrorHandler = require('./middleware/ErrorHandllingMiddleware.js')
+// const path = require('path')
 
 const PORT = process.env.PORT || 5000
-console.log("asdasdasdsad")
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(express.static(path.resolve(__dirname,'static')))
-app.use(fileUpload({}))
-app.use('/api',router)
+
+// const app = express()
+// app.use(cors())
+// app.use(express.json())
+// app.use(express.static(path.resolve(__dirname,'static')))
+// app.use(fileUpload({}))
+// app.use('/api',router)
 
 // Перевірка помилок, останій Middleware
-app.use(ErrorHandler)
+// app.use(ErrorHandler)
 
 
 const start = async ()=>{
 	try{
-		console.log("asdasdasdsad")
-		await sequelize.authenticate()
-		await sequelize.sync()
+		
+		// await sequelize.authenticate()
+		// await sequelize.sync()
 		app.listen(PORT,()=>console.log('SERVER START PORT - '+PORT))
 	}
 	catch(e){

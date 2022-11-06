6ba4467b1d5301a17d1e6324c26ec7899b2416b8
Donnchadh228 <117577537+Donnchadh228@users.noreply.github.com>
2022-11-07 00:10:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,19 @@
 require('dotenv').config()
-
 const express = require('express')
-// const sequelize=require('./db.js')
-// const models = require('./models/models.js')
-// const cors = require('cors')
-// const fileUpload = require('express-fileupload')
-// const router = require('./router/index.js')
-// const ErrorHandler = require('./middleware/ErrorHandllingMiddleware.js')
-// const path = require('path')
+
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
-// app.use(cors())
-// app.use(express.json())
-// app.use(express.static(path.resolve(__dirname,'static')))
-// app.use(fileUpload({}))
-// app.use('/api',router)
 
-// // Перевірка помилок, останій Middleware
-// app.use(ErrorHandler)
-console.log('TEST - '+PORT))
+app.get('/',(req,res)=>{
+	console.log(req.query)
+	return 0;
+})
 
 const start = async ()=>{
 	try{
-		
-// 		await sequelize.authenticate()
-// 		await sequelize.sync()
+	
 		app.listen(PORT,()=>console.log('SERVER START PORT - '+PORT))
 	}
 	catch(e){
